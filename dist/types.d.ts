8aae1c8e27a0ecc1a1ba10c24b7f377f37edcd0d
Connor Forbes <connorforbes25@gmail.com>
2025-04-11 17:11:09 +1000

    1.0.7: Move types to .ts so its included at build

---
+++ dist/types.d.ts
@@ -0,0 +1,39 @@
+import { PostgrestError, AuthError, SupabaseClient } from '@supabase/supabase-js';
+import { Options as PRetryOptions } from 'p-retry';
+type SupabaseResponse<T> = {
+    data: T | null;
+    error: PostgrestError | AuthError | null;
+};
+export type SupabaseyCallback<T = any> = (supabase: SupabaseClient) => Promise<SupabaseResponse<T>>;
+export interface SupabaseyOptions {
+    session: string | SupabaseClient;
+    retries?: number;
+    retry?: PRetryOptions;
+}
+interface SupabaseyInitOptions {
+    env?: Record<string, string | undefined>;
+    init?: boolean;
+    login?: boolean;
+    session?: 'auto' | string;
+}
+interface SupabaseyMiddlewareOptions extends SupabaseyInitOptions {
+    injectEnv?: boolean;
+}
+type ExtendedSupabaseClient = SupabaseClient & {
+    supabaseyIsLoggedIn?: boolean;
+};
+type SupabaseySessions = Record<string, ExtendedSupabaseClient>;
+export type BoundSupabaseyFunction = <T = any>(cb: SupabaseyCallback<T>, options?: Omit<SupabaseyOptions, 'session'>) => Promise<T>;
+export interface SupabaseyCallable extends SupabaseyStatics {
+    <T = any>(cb: SupabaseyCallback<T>, options: SupabaseyOptions): Promise<T>;
+}
+interface SupabaseyStatics {
+    sessions: SupabaseySessions;
+    bindSession: (session: string | SupabaseClient) => BoundSupabaseyFunction;
+    init: (options?: SupabaseyInitOptions) => Promise<BoundSupabaseyFunction>;
+    middleware: (options?: SupabaseyMiddlewareOptions) => (req: any, res: any, env: any, next?: any) => Promise<void>;
+    rpc: <T = any>(method: string, args?: Record<string, any>, options?: SupabaseyOptions) => Promise<T>;
+    throw: (err: any) => never;
+}
+export {};
+//# sourceMappingURL=types.d.ts.map
