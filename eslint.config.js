3910017d8018e74fe8c091a387619db12e3f7257
Matt Carter <m@ttcarter.com>
2025-07-17 13:26:30 +1000

    chore: Bump eslint to latest

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
+import {defineConfig, globalIgnores} from "eslint/config";
 import RulesMFDC from '@momsfriendlydevco/eslint-config';
 
-export default [
-	{
-		// Global ignore rules - Do not add any other keys to this object
-		ignores: [
-			'.*',
-			'dist/',
-			'node_modules/',
-		],
-	},
+export default defineConfig([
+	globalIgnores([
+		'.*',
+		'docs/',
+		'dist/',
+		'node_modules/',
+		'public/',
+		'**/.wrangler/',
+	]),
 	...RulesMFDC,
-]
+])
